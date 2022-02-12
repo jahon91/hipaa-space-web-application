9a51860141af84c33ca16b15f8b955a5fbb8cf5d
jahon91 <jdadadjanov@gmail.com>
2022-02-12 03:56:44 -0500

    Tests were added

--- src/test/java/com/hipaaspace/tests/HomePageTest.java
+++ src/test/java/com/hipaaspace/tests/HomePageTest.java
@@ -1,4 +1,15 @@
 package com.hipaaspace.tests;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class HomePageTest {
+
+    @Test
+    public void firstTest(){
+
+        String str = "Test";
+
+        Assert.assertEquals("Test",str);
+    }
 }
--- src/test/java/com/hipaaspace/tests/LookUpTest.java
+++ src/test/java/com/hipaaspace/tests/LookUpTest.java
@@ -1,4 +1,15 @@
 package com.hipaaspace.tests;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class LookUpTest {
+
+    @Test
+    public void firstTest(){
+
+        String str = "Test";
+
+        Assert.assertEquals("Test",str);
+    }
 }
