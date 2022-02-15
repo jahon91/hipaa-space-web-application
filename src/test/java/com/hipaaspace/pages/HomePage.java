abff213c576f6a21d085d22f6d7683dd1ecee49d
jahon91 <jdadadjanov@gmail.com>
2022-02-14 22:43:26 -0500

    intentionally put an error in first Test Class

--- src/test/java/com/hipaaspace/pages/HomePage.java
+++ src/test/java/com/hipaaspace/pages/HomePage.java
@@ -1,4 +1,10 @@
 package com.hipaaspace.pages;
 
+import org.openqa.selenium.support.PageFactory;
+
 public class HomePage {
+
+    public HomePage(){
+       // PageFactory.initElements();
+    }
 }
